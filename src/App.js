cac606ef0090abf9e9a5018953178aaf8f9ef7e3
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2020-06-13 12:09:32 +0545

    Replacing class component of App.js with hooks implementation in this branch only. will merge to master later if needed

--- src/App.js
+++ src/App.js
@@ -1,45 +1,38 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import Person from './Person/Person'
 
-class App extends Component {
-  state = {
+const App = (props) => {
+  const [personsState, setPersonsState] = useState({
     persons: [
       { name: 'Ashish', age: 34 },
       { name: 'Puppy', age: 26 },
       { name: 'Puplet', age: 1 },
     ],
-  }
+  })
 
-  switchNameHandler = () => {
-    this.setState({
+  const switchNameHandler = () => {
+    setPersonsState({
       persons: [
         { name: 'Ashish Karki', age: 34 },
-        { name: 'Dhuppy', age: 26 },
+        { name: 'Dhuppy', age: parseInt(Math.random() * 100) },
         { name: 'Puplet', age: 100 },
       ],
     })
   }
 
-  render() {
-    return (
-      <div className='App'>
-        <h1>Hello from ashish</h1>
-        <p style={{ color: 'red' }}>Another element within root app</p>
+  return (
+    <div className='App'>
+      <h1>Hello from ashish</h1>
+      <p style={{ color: 'red' }}>Another element within root app</p>
 
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+      <button onClick={switchNameHandler}>Switch Name</button>
 
-        {this.state.persons.map((value) => {
-          return <Person name={value.name} age={value.age} key={value.name} />
-        })}
-      </div>
-    )
-    // return React.createElement(
-    //   'div',
-    //   { className: 'App' },
-    //   React.createElement('h1', null, "Hi, I'm a React App!!!")
-    // )
-  }
+      {personsState.persons.map((value) => {
+        return <Person name={value.name} age={value.age} key={value.name} />
+      })}
+    </div>
+  )
 }
 
 export default App
