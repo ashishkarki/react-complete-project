efc0821c6ae306350e67a90a147dc7bd3b91af5d
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2020-06-29 15:43:04 +0545

    Using styled-components package instead of Radium to style our apps

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react'
-import Radium, { StyleRoot } from 'radium'
+//import Radium, { StyleRoot } from 'radium'
+import styled from 'styled-components'
 
 import './App.css'
 import Person from './Person/Person.component'
+
+const StyledButton = styled.button`
+  background-color: ${props => props.buttonWhenPersonDisplayed ? 'lightpink' : 'lightgreen' };
+  font: inherit;
+  border: 5px groove aquamarine;
+  padding: 8px;
+  cursor: pointer;
+  margin: 6px 10px;
+  &:hover {
+    background-color: ${props => props.buttonWhenPersonDisplayed ? 'Aquamarine' : 'skyblue' };
+    color: brown;
+  }
+`
 
 class App extends Component {
   state = {
@@ -14,25 +28,25 @@ class App extends Component {
     showPersonDiv: true,
   }
 
-  switchPersonInfoHandler = ( someName ) => {
-    this.setState( {
+  switchPersonInfoHandler = (someName) => {
+    this.setState({
       persons: [
         { name: someName, age: 34 },
-        { name: 'Dhuppy', age: Math.round( Math.random() * 100, 0 ) },
+        { name: 'Dhuppy', age: Math.round(Math.random() * 100, 0) },
         { name: 'Puplet', age: 100 },
       ],
-    } )
+    })
   }
 
-  deletePersonHandler = ( personIndex ) => {
+  deletePersonHandler = (personIndex) => {
     //const tempPersons = this.state.persons.slice() // returns a copy
     const tempPersons = [ ...this.state.persons ]
-    tempPersons.splice( personIndex, 1 )
-    this.setState( { persons: tempPersons } )
+    tempPersons.splice(personIndex, 1)
+    this.setState({ persons: tempPersons })
   }
 
-  nameChangedHandler = ( event, id ) => {
-    const toUpdatedPersonIdx = this.state.persons.findIndex( person => person.id === id )
+  nameChangedHandler = (event, id) => {
+    const toUpdatedPersonIdx = this.state.persons.findIndex(person => person.id === id)
     const toUpdatedPerson = {
       ...this.state.persons[ toUpdatedPersonIdx ]
     }
@@ -41,79 +55,81 @@ class App extends Component {
     const tempPersons = [ ...this.state.persons ]
     tempPersons[ toUpdatedPersonIdx ] = toUpdatedPerson
 
-    this.setState( {
+    this.setState({
       persons: tempPersons,
-    } )
+    })
   }
 
-  togglePersonHandler = ( _ ) => {
-    this.setState( {
+  togglePersonHandler = (_) => {
+    this.setState({
       showPersonDiv: !this.state.showPersonDiv,
-    } )
+    })
   }
 
-  render () {
-    const buttonStyle = {
-      backgroundColor: 'lightgreen',
-      font: 'inherit',
-      border: '5px groove aquamarine',
-      padding: '8px',
-      cursor: 'pointer',
-      margin: '6px 10px',
-      ':hover': {
-        backgroundColor: 'skyblue',
-        color: 'brown',
-      }
-    }
+  render() {
+    // const buttonStyle = {
+    //   backgroundColor: 'lightgreen',
+    //   font: 'inherit',
+    //   border: '5px groove aquamarine',
+    //   padding: '8px',
+    //   cursor: 'pointer',
+    //   margin: '6px 10px',
+    //   ':hover': {
+    //     backgroundColor: 'skyblue',
+    //     color: 'brown',
+    //   }
+    // }
 
     let personDiv = null
 
-    if ( this.state.showPersonDiv ) {
+    if (this.state.showPersonDiv) {
       personDiv = (
         <div>
-          { this.state.persons.map( ( person, index ) => (
+          { this.state.persons.map((person, index) => (
             <Person
               name={ person.name }
               age={ person.age }
               key={ person.id }
-              clicked={ ( _ ) => this.switchPersonInfoHandler( 'Fancy Ashish' ) }
-              deleteClicked={ () => this.deletePersonHandler( index ) }
-              inputChanged={ ( event ) => this.nameChangedHandler( event, person.id ) }
+              clicked={ (_) => this.switchPersonInfoHandler('Fancy Ashish') }
+              deleteClicked={ () => this.deletePersonHandler(index) }
+              inputChanged={ (event) => this.nameChangedHandler(event, person.id) }
             />
-          ) ) }
+          )) }
         </div>
       )
 
-      buttonStyle.backgroundColor = 'lightpink'
-      buttonStyle[ ':hover' ] = {
-        backgroundColor: 'ghostwhite',
-        color: 'black'
-      }
+      // buttonStyle.backgroundColor = 'lightpink'
+      // buttonStyle[ ':hover' ] = {
+      //   backgroundColor: 'ghostwhite',
+      //   color: 'black'
+      // }
+      // USING STYLED COMPONENTS
     }
 
     const classes = []
-    if ( this.state.persons.length <= 2 ) {
-      classes.push( 'red' )
+    if (this.state.persons.length <= 2) {
+      classes.push('red')
     }
-    if ( this.state.persons.length <= 1 ) {
-      classes.push( 'bold' )
+    if (this.state.persons.length <= 1) {
+      classes.push('bold')
     }
 
     return (
-      <StyleRoot>
-        <div className='App'>
-          <h1>Hello from ashish</h1>
-          <p className={ classes.join( ' ' ) }>Another element within root app</p>
+      //<StyleRoot>
+      <div className='App'>
+        <h1>Hello from ashish</h1>
+        <p className={ classes.join(' ') }>Another element within root app</p>
 
-          <button key={ 111 } onClick={ this.switchPersonInfoHandler.bind( this, 'Dancy Ashish' ) } style={ buttonStyle }>Switch Name</button>
+        <StyledButton key={ 111 } onClick={ this.switchPersonInfoHandler.bind(this, 'Dancy Ashish') } >Switch Name</StyledButton>
+        {/* style={ buttonStyle } */ }
 
-          <button key={ 222 } style={ buttonStyle } onClick={ this.togglePersonHandler }> { this.state.showPersonDiv ? 'Hide' : 'Show' } Person Div</button>
+        <StyledButton key={ 222 } buttonWhenPersonDisplayed={ this.state.showPersonDiv } onClick={ this.togglePersonHandler }> { this.state.showPersonDiv ? 'Hide' : 'Show' } Person Div</StyledButton>
 
-          {/* refer to https://react-cn.github.io/react/tips/if-else-in-JSX.html
+        {/* refer to https://react-cn.github.io/react/tips/if-else-in-JSX.html
         */}
-          { personDiv }
-        </div>
-      </StyleRoot>
+        { personDiv }
+      </div>
+      //</StyleRoot>
     )
     // return React.createElement(
     //   'div',
@@ -123,4 +139,5 @@ class App extends Component {
   }
 }
 
-export default Radium( App )
+// export default Radium( App )
+export default App
--- src/Person/Person.component.jsx
+++ src/Person/Person.component.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
-import Radium from 'radium'
+// import Radium from 'radium'
+import styled from 'styled-components'
 
 import './Person.styles.css'
 
-const person = ( props ) => {
+const StyledPersonDiv = styled.div`
+  width:  60%;
+  margin: 10px auto;
+  border:  2px solid blanchedalmond;
+  padding: 12px;
+  text-align: center;
+  box-shadow: 5px 2px 6px rgb(228, 189, 189);
+
+  @media (min-width: 700px) {
+    background-color: blanchedalmond;
+  }
+    `
+
+const person = (props) => {
   const deleteButtonStyle = {
     backgroundColor: 'pink',
     border: '5px groove salmon',
@@ -12,18 +26,19 @@ const person = ( props ) => {
     display: 'block',
   }
 
-  const radiumPersonStyle = {
-    '@media (min-width: 700px)': {
-      width: '450px',
-      backgroundColor: 'rgb(103, 197, 185)',
-    }
-  }
+  // const radiumPersonStyle = {
+  //   '@media (min-width: 700px)': {
+  //     width: '450px',
+  //     backgroundColor: 'rgb(103, 197, 185)',
+  //   }
+  // }
 
   return (
-    <div className='Person' style={ radiumPersonStyle }>
-      <p onClick={ props.clicked }>
-        This is a Person with { props.name } with age: { props.age }. You can click on me to update the person information.
-      </p>
+    //<div className='Person' style={ radiumPersonStyle }>
+    <StyledPersonDiv>
+      < p onClick={ props.clicked } >
+        This is a Person with { props.name } with age: { props.age }.You can click on me to update the person information.
+      </p >
       <p>
         { props.children && <strong>. Children are: { props.children }</strong> }
       </p>
@@ -31,8 +46,10 @@ const person = ( props ) => {
       <input type="text" onChange={ props.inputChanged } value={ props.name } />
 
       <button style={ deleteButtonStyle } onClick={ props.deleteClicked }>Delete Me</button>
-    </div>
+    </StyledPersonDiv>
+    //</div >
   )
 }
 
-export default Radium( person )
+//export default Radium( person )
+export default person
